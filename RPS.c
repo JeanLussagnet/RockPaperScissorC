a911e47c1546c58ba9db4d87bd4ca748914b78ad
KuRioSTmp <112512874+KuRioSTmp@users.noreply.github.com>
2024-04-22 11:38:06 +0200

    Functioning RPS
    Finished simple RPS

--- RPS.c
+++ RPS.c
@@ -4,22 +4,23 @@
 
 int startPlaying() {
     int choice;
-    printf("Playing Rock, Paper, Scisscors...");
-    printf("Select 1. Rock, 2. Paper, 3. Scisscors");
+    printf("Playing Rock, Paper, Scisscors...\n");
+    printf("Select 1. Rock, 2. Paper, 3. Scisscors\n");
 
-    printf("Select value...");
+    printf("Select value: \n");
     scanf("%d", &choice);
-    if (choice == 1)
-      printf("You have selected Rock!");
-    else if (choice == 2)
-      printf("You have selected Paper!");
-    else
-     printf("You have selected Scisscors!");
+    if (choice < 1 || choice > 3) {
+      printf("invalid choice! Please select a number between 1 - 3 \n");
+      return -1;
+    }
 
     return choice;
   }
 
-char computerChoice()
+char* getChoiceString(int choice) {
+  char *choices[] = {"Rock", "Paper", "Scisscors"};
+  return choices[choice - 1];
+} 
 
 int generateRandomNumber() {
   return (rand() % 3) + 1;
@@ -29,15 +30,23 @@ int main() {
   srand(time(NULL));
 
   int aiChoice = generateRandomNumber();
-
   int userChoice = startPlaying();
 
-  if (aiChoice == 1)
-    aiChoice == "Rock";
-  else if (aiChoice == 2)
-    aiChoice == "Paper";
-  else
-    aiChoice == "Scisscors";
+ if(userChoice != -1)
+  printf("You have selected %s\n", getChoiceString(userChoice));
+  printf("Computer has selected %s\n", getChoiceString(aiChoice));
 
-  printf("Computer picked ", "%s", aiChoice);
+  if (userChoice == aiChoice) {
+    printf("It's a tie!");
+  } else if ((userChoice == 1 && aiChoice == 3) ||
+            (userChoice == 2 && aiChoice == 1) ||
+            (userChoice == 3 && aiChoice == 2)) 
+            {
+              printf("You Win!!\n");
+            }
+  else {
+    printf("Computer Wins!\n");
+  }
+
+  return 0;
 }
